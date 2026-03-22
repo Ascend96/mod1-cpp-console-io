a329dc8c9c64776df1cb2a35a8a1f8363c444c3a
Mathew <mmartens28@yahoo.com>
2026-03-22 13:36:37 -0500

    update main.cpp to use getline for all inputs

--- DisplayInputsConsole/main.cpp
+++ DisplayInputsConsole/main.cpp
@@ -10,15 +10,15 @@ int main() {
     cout << "The goal is to take inputs and display data to the console for the user." << endl;
 
     cout << "Please enter a first name: ";
-    cin >> firstName;
+    getline(cin, firstName);
     cout << "Please enter a last name: ";
-    cin >> lastName;
+    getline(cin, lastName);
     cout << "Please enter a street address: ";
-    getline(cin, streetAddress); // use getline here because we want to allow spaces
+    getline(cin, streetAddress);
     cout << "Please enter a city: ";
-    getline(cin, city); // use getline since city might have spaces
+    getline(cin, city);
     cout << "Please enter a zip code: ";
-    cin >> zip;
+    getline(cin, zip);
 
     cout << "Here is your information: " << endl;
     cout << "First Name: " << firstName << endl;
@@ -28,6 +28,7 @@ int main() {
     cout << "Zip Code: " << zip << endl;
 
     // Wait for output
+    cout << endl << "Press any key to exit.";
     getch();
 
     return 0;
